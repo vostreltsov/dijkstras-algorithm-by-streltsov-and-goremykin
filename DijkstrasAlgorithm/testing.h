b53c7f6229eccc532e3bb7556edb8f3ba7c30542
vostreltsov <vostreltsov@gmail.com>
2012-02-17 13:44:35 +0300

    ????????? ?????.

--- DijkstrasAlgorithm/testing.h
+++ DijkstrasAlgorithm/testing.h
@@ -30,6 +30,7 @@ public:
 	{
 		Graph G;
 		std::vector<Edge> edges;
+
 		G.build(1, edges);
 		G.startNode = &G.nodes[0];
 		G.endNode = &G.nodes[0];
@@ -45,19 +46,74 @@ public:
 		std::vector<Edge> edges;
 		edges.push_back(Edge((Node *)0, (Node *)1, 15));
 		edges.push_back(Edge((Node *)0, (Node *)1, 10));
+
 		G.build(2, edges);
 		G.startNode = &G.nodes[0];
 		G.endNode = &G.nodes[1];
 		ExecutionState res = G.run();
+
 		assertTrue(res.totalWeight == 10, "Неверная длина пути (тест № 1)");
 		assertTrue(res.path.size() == 1, "Неверное количество переходов (тест № 1)");
 		assertTrue(res.path[0].weight == 10, "Неправильный переход (тест № 1)");		
+	}
+
+	// Тест из описания алгоритма на Википедии.
+	void test2()
+	{
+		Graph G;
+		std::vector<Edge> edges;
+		edges.push_back(Edge((Node *)0, (Node *)1, 7));
+		edges.push_back(Edge((Node *)0, (Node *)2, 9));
+		edges.push_back(Edge((Node *)0, (Node *)5, 14));
+		edges.push_back(Edge((Node *)1, (Node *)2, 10));
+		edges.push_back(Edge((Node *)1, (Node *)3, 15));
+		edges.push_back(Edge((Node *)2, (Node *)3, 11));
+		edges.push_back(Edge((Node *)2, (Node *)5, 2));
+		edges.push_back(Edge((Node *)3, (Node *)4, 6));
+		edges.push_back(Edge((Node *)5, (Node *)4, 9));
+
+		G.build(6, edges);
+		G.startNode = &G.nodes[0];
+		G.endNode = &G.nodes[5];
+		ExecutionState res = G.run();
+		assertTrue(res.totalWeight == 11, "Неверная длина пути (тест № 2)");
+		assertTrue(res.path.size() == 2, "Неверное количество переходов (тест № 2)");
+		assertTrue(res.path[0].weight == 9 && res.path[1].weight == 2, "Найдены неправильные переходы (тест № 2)");		
+
+		G.startNode = &G.nodes[0];
+		G.endNode = &G.nodes[3];
+		res = G.run();
+		assertTrue(res.totalWeight == 20, "Неверная длина пути (тест № 2)");
+		assertTrue(res.path.size() == 2, "Неверное количество переходов (тест № 2)");
+		assertTrue(res.path[0].weight == 9 && res.path[1].weight == 11, "Найдены неправильные переходы (тест № 2)");	
+	}
+
+	// Путь до одной из вершин напрямую длинее, чем в обход.
+	void test3()
+	{
+		Graph G;
+		std::vector<Edge> edges;
+		edges.push_back(Edge((Node *)0, (Node *)1, 10));
+		edges.push_back(Edge((Node *)0, (Node *)2, 1));
+		edges.push_back(Edge((Node *)1, (Node *)3, 5));
+		edges.push_back(Edge((Node *)2, (Node *)1, 2));
+		edges.push_back(Edge((Node *)2, (Node *)3, 20));
+
+		G.build(4, edges);
+		G.startNode = &G.nodes[0];
+		G.endNode = &G.nodes[3];
+		ExecutionState res = G.run();
+		assertTrue(res.totalWeight == 8, "Неверная длина пути (тест № 3)");
+		assertTrue(res.path.size() == 3, "Неверное количество переходов (тест № 3)");
+		assertTrue(res.path[0].weight == 1 && res.path[1].weight == 2 && res.path[2].weight == 5, "Найдены неправильные переходы (тест № 3)");			
 	}
 
 	void run()
 	{
 		test0();
 		test1();
+		test2();
+		test3();
 		printf("\nTesting complete: %d passes and %d fails.", passCount, failCount);
 	}
 };
